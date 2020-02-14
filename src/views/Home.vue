9916528eaefb98d4e575f959b92e03b212530399
iloveMeow99 <282568728@qq.com>
2020-02-14 22:18:07 +0800

    网络请求方法封装和首页轮播

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">Meow Shopping</div></nav-bar>
+    <swiper :bannerList="bannerList" />
   </div>
 </template>
 
 <script>
 import NavBar from '@/components/NavBar'
+import Swiper from './home/Swiper'
+import { getHomeMultidata } from '@/network/home'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    Swiper
+  },
+  data () {
+    return {
+      bannerList: [],
+      recommendList: []
+    }
+  },
+  created () {
+    getHomeMultidata().then(res => {
+      // console.log(res.data)
+      this.bannerList = res.data.banner.list
+      this.recommendList = res.data.recommend.list
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
+#home{
+  width: 100%;
   .home-nav{
-    background-color: var(--color-tint);
+    background-color: #d4237a;
     color: #ffffff;
   }
+}
 </style>
---
+++ src/views/home/Swiper.vue
@@ -0,0 +1,28 @@
+<template>
+  <van-swipe :autoplay="3000" indicator-color="#d4237a">
+    <van-swipe-item v-for="(item, index) in bannerList" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" />
+      </a>
+    </van-swipe-item>
+  </van-swipe>
+</template>
+
+<script>
+export default {
+  props: {
+    bannerList: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .van-swipe img{
+    width: 100%;
+  }
+</style>
